28c77cab82f95e09779abd56075b87049bf5a9f2
Sifan Geremu <wins33975@gmail.com>
2026-03-31 10:19:15 +0300

    style the active current link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import {usePathname}  from 'next/navigation'
+import classnames from 'classnames'
 import { AiOutlineIssuesClose } from "react-icons/ai";
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label:"Dashboard",href:"/dashboard"},
     { label:"issues",href:"/issues"},
@@ -9,13 +13,16 @@ const NavBar = () => {
   ]
   return (
     
-   <nav className='flex space-x-6 border-b-3   mb-4 px-4 h-14 items-center'>
-    <Link href='/'>
-   <AiOutlineIssuesClose />
+   <nav className='flex space-x-6 border-b-3   mb-4 px-4 h-14 items-center bg-background'>
+    <Link  href='/'>
+   <AiOutlineIssuesClose  className='text-3xl'/>
     </Link>
     <ul className='flex space-x-6'>
       {links.map((link)=>(
-        <li key={link.href}>
+        <li key={link.href} className={classnames({'text-zinc-900':link.href===currentPath,
+          'text-zinc-500':link.href !== currentPath,
+          'hover:text-zinc-800 transition-colors':true
+})}>
           <Link href={link.href}>
             {link.label}
           </Link>
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const issuesPage = () => {
+  return (
+    <div>
+      issues page
+    </div>
+  )
+}
+
+export default issuesPage
