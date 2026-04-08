8885e6cc84c7f4556dad2429d1c35ab8334c6aa1
Sifan Geremu <wins33975@gmail.com>
2026-04-08 23:48:46 +0300

    feat: add TextArea component to issues page for issue description input

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import React from 'react'
 import { Button } from '@radix-ui/themes'
+import { TextArea } from '@radix-ui/themes'
 import Link from 'next/link'
 const issuesPage = () => {
   return (
     <div>
       <Button ><Link href="/issues/new">New Issue</Link></Button>
+      <TextArea></TextArea>
     </div>
   )
 }
